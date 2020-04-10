940d996bcc2907d8d905ded5b48708225fc407b0
Rohan Bhatia <mr.bhatia07@gmail.com>
2020-04-11 02:51:35 +0530

    Added quiz end logic

--- app/screens/Quiz/Quiz.js
+++ app/screens/Quiz/Quiz.js
@@ -3,17 +3,19 @@ import QuizHeader from '../../components/QuizHeader';
 import QuizQuestion from '../../components/QuizQuestion';
 import QuizButton from '../../components/QuizButton';
 import {View} from 'react-native';
+import {Image, Text, Button} from 'react-native-elements';
+import quizzy from '../../assets/quizzy.png';
 import styles from './styles';
 import questions from '../../questions';
 const Quiz = props => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const questionsArray = questions.questionsArray;
   const QuizButtonPressed = () => {
-    if (currentIndex === questionsArray.length - 1) {
+    if (currentIndex === questionsArray.length) {
       console.log('submit quiz');
     }
 
-    if (currentIndex < questionsArray.length - 1) {
+    if (currentIndex < questionsArray.length) {
       setCurrentIndex(currentIndex + 1);
     }
   };
@@ -26,15 +28,48 @@ const Quiz = props => {
           setQuizStarted={props.setQuizStarted}
         />
       </View>
-      <View style={styles.questionContainer}>
-        <QuizQuestion
-          question={questionsArray[currentIndex].question}
-          options={questionsArray[currentIndex].options}
-        />
-      </View>
-      <View style={styles.buttonContainer}>
-        <QuizButton title="Check" onPress={QuizButtonPressed} />
-      </View>
+      {currentIndex < questionsArray.length && (
+        <View style={styles.mainQuizContainer}>
+          <View style={styles.questionContainer}>
+            <QuizQuestion
+              question={questionsArray[currentIndex].question}
+              options={questionsArray[currentIndex].options}
+            />
+          </View>
+          <View style={styles.buttonContainer}>
+            <QuizButton title="Check" onPress={QuizButtonPressed} />
+          </View>
+        </View>
+      )}
+      {currentIndex === questionsArray.length && (
+        <View style={styles.quizEndContainer}>
+          <View style={styles.quizEndMessage}>
+            <Image source={quizzy} style={styles.quizzyImage} />
+            <Text style={styles.endText}>
+              Congratulations! You have completed the quiz.{'\n\n'}Do You want
+              to retake the quiz?
+            </Text>
+          </View>
+          <View style={styles.quizEndButtons}>
+            <Button
+              buttonStyle={styles.quizEndButton}
+              titleStyle={styles.quizEndButtonTitle}
+              title="Retake Quiz"
+              onPress={() => {
+                setCurrentIndex(0);
+              }}
+            />
+            <Button
+              buttonStyle={styles.quizEndButton}
+              titleStyle={styles.quizEndButtonTitle}
+              title="End Quiz"
+              onPress={() => {
+                props.setQuizStarted(false);
+              }}
+            />
+          </View>
+        </View>
+      )}
     </View>
   );
 };
--- app/screens/Quiz/styles.js
+++ app/screens/Quiz/styles.js
@@ -8,13 +8,52 @@ const styles = StyleSheet.create({
   headerContainer: {
     flex: 0.1,
   },
+  mainQuizContainer: {
+    flex: 0.9,
+  },
   questionContainer: {
     flex: 0.7,
   },
   buttonContainer: {
-    flex: 0.2,
+    flex: 0.3,
     justifyContent: 'flex-end',
   },
+  questionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    color: '#3B0D11',
+  },
+  quizEndContainer: {
+    flex: 0.7,
+    padding: 20,
+    justifyContent: 'center',
+  },
+  endText: {
+    flex: 1,
+    fontSize: 18,
+    color: '#3B0D11',
+  },
+  quizEndButton: {
+    marginTop: 25,
+    backgroundColor: '#6D435A',
+    padding: 15,
+    elevation: 5,
+    borderRadius: 7,
+  },
+  quizEndButtonTitle: {
+    color: '#FFFCF9',
+  },
+  quizzyImage: {
+    width: 175,
+    height: 175,
+    marginBottom: 25,
+  },
+  quizEndMessage: {
+    flex: 0.6,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  quizEndButtons: {},
 });
 
 export default styles;
